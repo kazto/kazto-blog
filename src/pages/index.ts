4d49e0f669a930f51919ac41b4c0d884b680885d
TAKAHASHI Kazto <kazuto.takahashi@thinkings.co.jp>
2024-01-10 11:44:47 +0900

    add husky

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,20 +2,20 @@ import { Context, Hono } from "hono";
 import { Env } from "../index";
 
 export type Title = {
-	post_title: string;
-	post_date: string;
+    post_title: string;
+    post_date: string;
 };
 
 export const index = async (
-	c: Context<
-		{
-			Bindings: Env;
-		},
-		"/",
-		{}
-	>,
+    c: Context<
+        {
+            Bindings: Env;
+        },
+        "/",
+        {}
+    >,
 ) => {
-	const sql = `
+    const sql = `
     SELECT
       post_title, post_date
     FROM
@@ -24,14 +24,12 @@ export const index = async (
       post_type = 'post'
     ORDER BY post_date DESC;
     `;
-	const { results }: D1Result<Title> = await c.env?.DB?.prepare(sql)?.all();
-	return c.html(
-		results
-			.map((r) => {
-				return `<p><a href="/posts/${r.post_date.replace(/[-: ]/g, "")}">${
-					r.post_title
-				}</a></p>`;
-			})
-			.join(""),
-	);
+    const { results }: D1Result<Title> = await c.env?.DB?.prepare(sql)?.all();
+    return c.html(
+        results
+            .map((r) => {
+                return `<p><a href="/posts/${r.post_date.replace(/[-: ]/g, "")}">${r.post_title}</a></p>`;
+            })
+            .join(""),
+    );
 };
